90a70fbd0fcf10883534b00083f495a7e14b13b5
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2020-06-10 16:47:51 +0500

    Comments for functions in file_utils.h

--- src/file_utils.cpp
+++ src/file_utils.cpp
@@ -1,10 +1,10 @@
 #include "file_utils.h"
 
-#include <iomanip>
 #include <cstring>
 #include <dirent.h>
+#include <iomanip>
 
-TNoDirError::TNoDirError(const std::string& msg): std::runtime_error(msg) {}
+TNoDirError::TNoDirError(const std::string& msg) : std::runtime_error(msg) {}
 
 bool TryOpen(const std::vector<std::string>& fnames, std::ifstream& file)
 {
@@ -25,21 +25,25 @@ void WriteToFile(const std::string& fileName, const std::string& value)
     f << value;
 }
 
-std::string IterateDir(const std::string& dirName, const std::string& pattern, std::function<bool (const std::string& )> fn)
+std::string IterateDir(const std::string&                      dirName,
+                       const std::string&                      pattern,
+                       std::function<bool(const std::string&)> fn)
 {
     DIR* dir = opendir(dirName.c_str());
 
-    if(dir == NULL) {
+    if (dir == NULL) {
         throw TNoDirError("Can't open directory: " + dirName);
     }
-    auto closeFn = [](DIR* d) { closedir(d); };
+
+    dirent*                                 ent;
+    auto                                    closeFn = [](DIR* d) { closedir(d); };
     std::unique_ptr<DIR, decltype(closeFn)> dirPtr(dir, closeFn);
-    dirent* ent;
-    while ((ent = readdir (dirPtr.get())) != NULL) {
-        if (!std::strstr(ent->d_name, pattern.c_str()))
+    while ((ent = readdir(dirPtr.get())) != NULL) {
+        if (!std::strstr(ent->d_name, pattern.c_str())) {
             continue;
+        }
         std::string d(dirName + "/" + std::string(ent->d_name));
-        if(fn(d)){
+        if (fn(d)) {
             return d;
         }
     }
--- src/file_utils.h
+++ src/file_utils.h
@@ -1,13 +1,27 @@
 #pragma once
 
+#include <fstream>
 #include <functional>
 #include <vector>
-#include <fstream>
 
+/**
+ * @brief Try to open one of files listed in fnames.
+ *
+ * @param fnames Vector of filenames to try to open
+ * @param file Reference to ifstream of openeded file.
+ * @return true One of files is open succesfuly
+ * @return false Nothing is open
+ */
 bool TryOpen(const std::vector<std::string>& fnames, std::ifstream& file);
 
-
-template<class T> void OpenWithException(T& f, const std::string& fileName)
+/**
+ * @brief Open file. Throw exception on failure.
+ *
+ * @tparam T ifstream or ofstream
+ * @param f Stream to open
+ * @param fileName Name of file to open
+ */
+template <class T> void OpenWithException(T& f, const std::string& fileName)
 {
     f.open(fileName);
     if (!f.is_open()) {
@@ -15,15 +29,32 @@ template<class T> void OpenWithException(T& f, const std::string& fileName)
     }
 }
 
+/**
+ * @brief Write a string to file
+ *
+ * @param fileName Name of file
+ * @param value Value to write
+ */
 void WriteToFile(const std::string& fileName, const std::string& value);
 
 /**
  * @brief Exception class thrown on open directory failure.
  */
-class TNoDirError: public std::runtime_error
+class TNoDirError : public std::runtime_error
 {
-    public:
-        TNoDirError(const std::string& msg);
+public:
+    TNoDirError(const std::string& msg);
 };
 
-std::string IterateDir(const std::string& dirName, const std::string& pattern, std::function<bool (const std::string& )> fn);
+/**
+ * @brief Iterate over entries of the folder. If entry name contains pattern, execute fn with the
+ * entry.
+ *
+ * @param dirName Folder to iterate over
+ * @param pattern String what should be a part of folder entry to call fn
+ * @param fn The function calls fn with matching folder entry. If fn returns true, iteration stops.
+ * @return std::string Folder entry for which fn returned true or empty string
+ */
+std::string IterateDir(const std::string&                      dirName,
+                       const std::string&                      pattern,
+                       std::function<bool(const std::string&)> fn);
