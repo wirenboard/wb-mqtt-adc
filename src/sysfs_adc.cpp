aa7fca13ace161f0e8d76075f746b5242578fd52
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2020-06-10 18:07:04 +0500

    sysfs_adc format

--- src/sysfs_adc.cpp
+++ src/sysfs_adc.cpp
@@ -1,7 +1,7 @@
 #include "sysfs_adc.h"
 
-#include <math.h>
 #include <iomanip>
+#include <math.h>
 
 #include <fnmatch.h>
 #include <unistd.h>
@@ -15,7 +15,7 @@ namespace
     std::string FindBestScale(const std::vector<std::string>& scales, double desiredScale)
     {
         std::string bestScaleStr;
-        double bestScaleDouble = 0;
+        double      bestScaleDouble = 0;
 
         for (auto& scaleStr : scales) {
             double val;
@@ -25,13 +25,12 @@ namespace
                 continue;
             }
             // best scale is either maximum scale or the one closest to user request
-            if (((desiredScale > 0) && (fabs(val - desiredScale) <= fabs(bestScaleDouble - desiredScale)))      // user request
-                ||
-                ((desiredScale <= 0) && (val >= bestScaleDouble))      // maximum scale
-                )
-            {
+            if (((desiredScale > 0) &&
+                 (fabs(val - desiredScale) <= fabs(bestScaleDouble - desiredScale))) // user request
+                || ((desiredScale <= 0) && (val >= bestScaleDouble)) // maximum scale
+            ) {
                 bestScaleDouble = val;
-                bestScaleStr = scaleStr;
+                bestScaleStr    = scaleStr;
             }
         }
         return bestScaleStr;
@@ -44,16 +43,15 @@ namespace
         }
 
         std::string pattern = "*" + matchIIO + "*";
-        auto fn = [&](const std::string& d)
-            {
-                char buf[512];
-                int len;
-                if ((len = readlink(d.c_str(), buf, 512)) < 0)
-                    return false;
-                buf[len] = 0;
 
-                return (fnmatch(pattern.c_str(), buf, 0) == 0);
-            };
+        auto fn = [&](const std::string& d) {
+            char buf[512];
+            int  len;
+            if ((len = readlink(d.c_str(), buf, 512)) < 0)
+                return false;
+            buf[len] = 0;
+            return (fnmatch(pattern.c_str(), buf, 0) == 0);
+        };
 
         std::string iioDevName = IterateDir(sysFsPrefix + "/bus/iio/devices", "iio:device", fn);
 
@@ -63,23 +61,18 @@ namespace
 
         return iioDevName;
     }
-}
+} // namespace
 
-TChannelReader::TChannelReader(double defaultIIOScale, 
-                               uint32_t maxADCvalue, 
-                               const TChannelReader::TSettings& cfg, 
-                               uint32_t delayBetweenMeasurementsmS,
-                               WBMQTT::TLogger& debugLogger,
-                               WBMQTT::TLogger& infoLogger,
-                               const std::string& sysFsPrefix): 
-    Cfg(cfg),
-    MeasuredV(0.0),
-    IIOScale(defaultIIOScale),
-    MaxADCValue(maxADCvalue),
-    DelayBetweenMeasurementsmS(delayBetweenMeasurementsmS),
-    AverageCounter(cfg.AveragingWindow),
-    DebugLogger(debugLogger),
-    InfoLogger(infoLogger)
+TChannelReader::TChannelReader(double                           defaultIIOScale,
+                               uint32_t                         maxADCvalue,
+                               const TChannelReader::TSettings& cfg,
+                               uint32_t                         delayBetweenMeasurementsmS,
+                               WBMQTT::TLogger&                 debugLogger,
+                               WBMQTT::TLogger&                 infoLogger,
+                               const std::string&               sysFsPrefix)
+    : Cfg(cfg), MeasuredV(0.0), IIOScale(defaultIIOScale), MaxADCValue(maxADCvalue),
+      DelayBetweenMeasurementsmS(delayBetweenMeasurementsmS), AverageCounter(cfg.AveragingWindow),
+      DebugLogger(debugLogger), InfoLogger(infoLogger)
 {
     SysfsIIODir = FindSysfsIIODir(sysFsPrefix, cfg.MatchIIO);
     SelectScale();
@@ -104,24 +97,26 @@ void TChannelReader::Measure()
         std::this_thread::sleep_for(std::chrono::milliseconds(DelayBetweenMeasurementsmS));
     }
 
-    if(!AverageCounter.IsReady()) {
+    if (!AverageCounter.IsReady()) {
         DebugLogger.Log() << Cfg.ChannelNumber << " average is not ready";
         return;
     }
 
-    uint32_t value = AverageCounter.Average();
+    uint32_t value = AverageCounter.GetAverage();
     if (value > MaxADCValue) {
-        InfoLogger.Log() << Cfg.ChannelNumber << " average (" << value <<") is bigger than maximum (" << MaxADCValue <<")";
+        InfoLogger.Log() << Cfg.ChannelNumber << " average (" << value
+                         << ") is bigger than maximum (" << MaxADCValue << ")";
         return;
     }
 
     double v = IIOScale * value;
     if (v > Cfg.MaxScaledVoltage) {
-        InfoLogger.Log() << Cfg.ChannelNumber << " scaled value (" << v <<") is bigger than maximum (" << Cfg.MaxScaledVoltage <<")";
+        InfoLogger.Log() << Cfg.ChannelNumber << " scaled value (" << v
+                         << ") is bigger than maximum (" << Cfg.MaxScaledVoltage << ")";
         return;
     }
 
-    MeasuredV = v * Cfg.VoltageMultiplier / 1000.0;   //got mV let's divide it by 1000 to obtain V
+    MeasuredV = v * Cfg.VoltageMultiplier / 1000.0; // got mV let's divide it by 1000 to obtain V
 }
 
 int32_t TChannelReader::ReadFromADC()
@@ -137,18 +132,19 @@ void TChannelReader::SelectScale()
     std::string scalePrefix = SysfsIIODir + "/in_" + Cfg.ChannelNumber + "_scale";
 
     std::ifstream scaleFile;
-    TryOpen({ scalePrefix + "_available", 
-            SysfsIIODir + "/in_voltage_scale_available",
-            SysfsIIODir + "/scale_available"
-            }, scaleFile);
+    TryOpen({scalePrefix + "_available",
+             SysfsIIODir + "/in_voltage_scale_available",
+             SysfsIIODir + "/scale_available"},
+            scaleFile);
 
     if (scaleFile.is_open()) {
-        auto contents = std::string((std::istreambuf_iterator<char>(scaleFile)), std::istreambuf_iterator<char>());
+        auto contents = std::string((std::istreambuf_iterator<char>(scaleFile)),
+                                    std::istreambuf_iterator<char>());
         InfoLogger.Log() << "Available scales: " << contents;
 
         std::string bestScaleStr = FindBestScale(WBMQTT::StringSplit(contents, " "), Cfg.Scale);
 
-        if(!bestScaleStr.empty()) {
+        if (!bestScaleStr.empty()) {
             IIOScale = std::stod(bestScaleStr);
             WriteToFile(scalePrefix, bestScaleStr);
             InfoLogger.Log() << scalePrefix << " is set to " << bestScaleStr;
@@ -156,8 +152,9 @@ void TChannelReader::SelectScale()
         }
     }
 
-    // scale_available file is not present read the current scale(in_voltageX_scale) from sysfs or from group scale(in_voltage_scale)
-    TryOpen({ scalePrefix, SysfsIIODir + "/in_voltage_scale" }, scaleFile);
+    // scale_available file is not present read the current scale(in_voltageX_scale) from sysfs or
+    // from group scale(in_voltage_scale)
+    TryOpen({scalePrefix, SysfsIIODir + "/in_voltage_scale"}, scaleFile);
     if (scaleFile.is_open()) {
         scaleFile >> IIOScale;
     }
--- src/sysfs_adc.h
+++ src/sysfs_adc.h
@@ -13,81 +13,84 @@
  */
 class TChannelReader
 {
-    public:
-        //! ADC channel measurement settings
-        struct TSettings
-        {
-            std::string MatchIIO;                                    //! Fnmatch-compatible pattern to match with iio:deviceN symlink target
-            std::string ChannelNumber     = "voltage1";              //! IIO channel "voltageX"
-            uint32_t    ReadingsNumber    = 10;                      //! Number of value readings during one selection
-            double      MaxScaledVoltage  = ADC_DEFAULT_MAX_VOLTAGE; //! Maximum result after multiplying readed value from ADC to Scale
-            double      Scale             = 0;                       //! The ADC scale to use. The closest supported scale (from _scale_available list) will be used. It affects the accuracy and the measurement range. If 0, the maximum available scale is used.",
-            double      VoltageMultiplier = 1;                       //! The ADC voltage is multiplied by this factor to get the resulting value
-            uint32_t    AveragingWindow   = 10;                      //! Number of consecutive readings to average
-            uint32_t    DecimalPlaces     = 3;                       //! Number of figures after point
-        };
+public:
+    //! ADC channel measurement settings
+    struct TSettings
+    {
+        //! Fnmatch-compatible pattern to match with iio:deviceN symlink target
+        std::string MatchIIO;
+        std::string ChannelNumber    = "voltage1"; //! IIO channel "voltageX"
+        uint32_t    ReadingsNumber   = 10;         //! Number of value readings during one selection
+        double      MaxScaledVoltage = ADC_DEFAULT_MAX_VOLTAGE; //! Maximum result after multiplying
+                                                                //! readed value from ADC to Scale
+        double Scale = 0; //! The ADC scale to use. The closest supported scale (from
+                          //! _scale_available list) will be used. It affects the accuracy and the
+                          //! measurement range. If 0, the maximum available scale is used.",
+        double VoltageMultiplier =
+            1; //! The ADC voltage is multiplied by this factor to get the resulting value
+        uint32_t AveragingWindow = 10; //! Number of consecutive readings to average
+        uint32_t DecimalPlaces   = 3;  //! Number of figures after point
+    };
 
-        /**
-         * @brief Construct a new TChannelReader object
-         * 
-         * @param defaultIIOScale 
-         * @param maxADCvalue 
-         * @param channelCfg 
-         * @param delayBetweenMeasurementsmS 
-         * @param debugLogger 
-         * @param sysFsPrefix 
-         */
-        TChannelReader(double defaultIIOScale, 
-                       uint32_t maxADCvalue, 
-                       const TChannelReader::TSettings& channelCfg, 
-                       uint32_t delayBetweenMeasurementsmS, 
-                       WBMQTT::TLogger& debugLogger,
-                       WBMQTT::TLogger& infoLogger,
-                       const std::string& sysFsPrefix = "/sys");
+    /**
+     * @brief Construct a new TChannelReader object
+     *
+     * @param defaultIIOScale
+     * @param maxADCvalue
+     * @param channelCfg
+     * @param delayBetweenMeasurementsmS
+     * @param debugLogger
+     * @param sysFsPrefix
+     */
+    TChannelReader(double                           defaultIIOScale,
+                   uint32_t                         maxADCvalue,
+                   const TChannelReader::TSettings& channelCfg,
+                   uint32_t                         delayBetweenMeasurementsmS,
+                   WBMQTT::TLogger&                 debugLogger,
+                   WBMQTT::TLogger&                 infoLogger,
+                   const std::string&               sysFsPrefix = "/sys");
 
-        std::string GetValue() const;
-        void Measure();
+    std::string GetValue() const;
+    void        Measure();
 
-    private:
-        //! Settings for the channel
-        TChannelReader::TSettings Cfg;
+private:
+    //! Settings for the channel
+    TChannelReader::TSettings Cfg;
 
-        //! Last measured voltage in V
-        double MeasuredV;
+    //! Last measured voltage in V
+    double MeasuredV;
 
-        //! Folder in sysfs corresponding to the channel
-        std::string  SysfsIIODir;
-        
-        /*! Selected scale for the channel. 
-        
-            The closest value from one of files
-            /bus/iio/devices/iio:device0/in_voltageX_scale_available 
-            /bus/iio/devices/iio:device0/in_voltage_scale_available
-            /bus/iio/devices/iio:device0/scale_available
+    //! Folder in sysfs corresponding to the channel
+    std::string SysfsIIODir;
 
-            Or the value from one of
-            /bus/iio/devices/iio:device0/in_voltageX_scale
-            /bus/iio/devices/iio:device0/in_voltage_scale
+    /*! Selected scale for the channel.
 
-            Or the value got from constructor's parameter defaultIIOScale.
-        */
-        double IIOScale;
+        The closest value from one of files
+        /bus/iio/devices/iio:device0/in_voltageX_scale_available
+        /bus/iio/devices/iio:device0/in_voltage_scale_available
+        /bus/iio/devices/iio:device0/scale_available
 
-        //! Maximum possible value from ADC
-        uint32_t MaxADCValue;
+        Or the value from one of
+        /bus/iio/devices/iio:device0/in_voltageX_scale
+        /bus/iio/devices/iio:device0/in_voltage_scale
 
-        //! Delay between measurements in mS
-        uint32_t DelayBetweenMeasurementsmS;
+        Or the value got from constructor's parameter defaultIIOScale.
+    */
+    double IIOScale;
 
-        TMovingAverageCalculator AverageCounter;
+    //! Maximum possible value from ADC
+    uint32_t MaxADCValue;
 
-        std::ifstream AdcValStream;
+    //! Delay between measurements in mS
+    uint32_t DelayBetweenMeasurementsmS;
 
-        WBMQTT::TLogger& DebugLogger;
-        WBMQTT::TLogger& InfoLogger;
+    TMovingAverageCalculator AverageCounter;
+    std::ifstream            AdcValStream;
+    WBMQTT::TLogger&         DebugLogger;
+    WBMQTT::TLogger&         InfoLogger;
 
-        int32_t ReadFromADC();
-        void SelectScale();
+    int32_t ReadFromADC();
+    void    SelectScale();
 
-        TChannelReader();
+    TChannelReader();
 };
