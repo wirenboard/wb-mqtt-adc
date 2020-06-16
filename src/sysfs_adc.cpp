2682ce09235e70147b981a7282e0f5a3cc67e043
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2020-06-16 14:39:42 +0500

    Added workaround for https://gcc.gnu.org/bugzilla/show_bug.cgi?id=53984

--- src/sysfs_adc.cpp
+++ src/sysfs_adc.cpp
@@ -16,9 +16,8 @@ TChannelReader::TChannelReader(double                           defaultIIOScale,
                                WBMQTT::TLogger&                 debugLogger,
                                WBMQTT::TLogger&                 infoLogger,
                                const std::string&               sysfsIIODir)
-    : Cfg(cfg), SysfsIIODir(sysfsIIODir), IIOScale(defaultIIOScale), MaxADCValue(maxADCvalue),
-      DelayBetweenMeasurementsmS(delayBetweenMeasurementsmS), AverageCounter(cfg.AveragingWindow),
-      DebugLogger(debugLogger)
+    : Cfg(cfg), SysfsIIODir(sysfsIIODir), IIOScale(defaultIIOScale), MaxADCValue(maxADCvalue), DelayBetweenMeasurementsmS(delayBetweenMeasurementsmS),
+      AverageCounter(cfg.AveragingWindow), DebugLogger(debugLogger)
 {
     SelectScale(infoLogger);
 }
@@ -44,14 +43,12 @@ void TChannelReader::Measure()
 
     uint32_t value = AverageCounter.GetAverage();
     if (value > MaxADCValue) {
-        throw std::runtime_error(Cfg.ChannelNumber + " average (" + std::to_string(value) +
-                                 ") is bigger than maximum (" + std::to_string(MaxADCValue) + ")");
+        throw std::runtime_error(Cfg.ChannelNumber + " average (" + std::to_string(value) + ") is bigger than maximum (" + std::to_string(MaxADCValue) + ")");
     }
 
     double v = IIOScale * value;
     if (v > Cfg.MaxScaledVoltage) {
-        throw std::runtime_error(Cfg.ChannelNumber + " scaled value (" + std::to_string(v) +
-                                 ") is bigger than maximum (" +
+        throw std::runtime_error(Cfg.ChannelNumber + " scaled value (" + std::to_string(v) + ") is bigger than maximum (" +
                                  std::to_string(Cfg.MaxScaledVoltage) + ")");
     }
 
@@ -68,10 +65,18 @@ int32_t TChannelReader::ReadFromADC()
     std::string   fileName = SysfsIIODir + "/in_" + Cfg.ChannelNumber + "_raw";
     OpenWithException(adcValStream, fileName);
     int32_t val;
-    adcValStream >> val;
-    if (adcValStream.fail())
-        throw std::runtime_error("Can't read from " + fileName);
-    return val;
+    for (size_t i = 0; i < 3; ++i) { // workaround for https://gcc.gnu.org/bugzilla/show_bug.cgi?id=53984
+        try {
+            adcValStream >> val;
+            if (adcValStream.good())
+                return val;
+        } catch (const std::underflow_error&) {
+        }
+        adcValStream.clear();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        adcValStream.seekg(0);
+    }
+    throw std::runtime_error("Can't read from " + fileName);
 }
 
 void TChannelReader::SelectScale(WBMQTT::TLogger& infoLogger)
@@ -79,18 +84,13 @@ void TChannelReader::SelectScale(WBMQTT::TLogger& infoLogger)
     std::string scalePrefix = SysfsIIODir + "/in_" + Cfg.ChannelNumber + "_scale";
 
     std::ifstream scaleFile;
-    TryOpen({scalePrefix + "_available",
-             SysfsIIODir + "/in_voltage_scale_available",
-             SysfsIIODir + "/scale_available"},
-            scaleFile);
+    TryOpen({scalePrefix + "_available", SysfsIIODir + "/in_voltage_scale_available", SysfsIIODir + "/scale_available"}, scaleFile);
 
     if (scaleFile.is_open()) {
-        auto contents = std::string((std::istreambuf_iterator<char>(scaleFile)),
-                                    std::istreambuf_iterator<char>());
+        auto contents = std::string((std::istreambuf_iterator<char>(scaleFile)), std::istreambuf_iterator<char>());
         infoLogger.Log() << "Available scales: " << contents;
 
-        std::string bestScaleStr =
-            FindBestScale(WBMQTT::StringSplit(contents, " "), Cfg.DesiredScale);
+        std::string bestScaleStr = FindBestScale(WBMQTT::StringSplit(contents, " "), Cfg.DesiredScale);
 
         if (!bestScaleStr.empty()) {
             IIOScale = std::stod(bestScaleStr);
@@ -142,9 +142,8 @@ std::string FindBestScale(const std::vector<std::string>& scales, double desired
             continue;
         }
         // best scale is either maximum scale or the one closest to user request
-        if (((desiredScale > 0) &&
-             (fabs(val - desiredScale) <= fabs(bestScaleDouble - desiredScale))) // user request
-            || ((desiredScale <= 0) && (val >= bestScaleDouble))                 // maximum scale
+        if (((desiredScale > 0) && (fabs(val - desiredScale) <= fabs(bestScaleDouble - desiredScale))) // user request
+            || ((desiredScale <= 0) && (val >= bestScaleDouble))                                       // maximum scale
         ) {
             bestScaleDouble = val;
             bestScaleStr    = scaleStr;
